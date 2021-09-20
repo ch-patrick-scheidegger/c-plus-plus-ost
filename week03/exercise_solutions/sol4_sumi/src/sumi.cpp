350e9ba4c17ae117d736831c7fd2e26b79aa4c45
Thomas Corbat <thomas.corbat@ost.ch>
2021-09-20 07:31:12 +0200

    Week 3: Make sumi compile with non-reduce implementation

--- week03/exercise_solutions/sol4_sumi/src/sumi.cpp
+++ week03/exercise_solutions/sol4_sumi/src/sumi.cpp
@@ -1,9 +1,11 @@
 #include "sumi.h"
+
 #include <istream>
 #include <iterator>
 #include <numeric>
+
 int sumi(std::istream &is) {
 	using input=std::istream_iterator<int>;
 	input eof{};
-	return std::reduce(input{is},eof,0);
+	return std::accumulate(input{is}, eof, 0); //if available std::reduce(input{is}, eof, 0);
 }
