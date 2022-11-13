9c00c8888503268f1fe9722e1840cf4c79e5351a
Felix Morgner <felix.morgner@ost.ch>
2022-11-13 21:12:48 +0100

    week08: modernize solutions

--- week08/exercise_solutions/sol04_anagrams/anagram.cpp
+++ week08/exercise_solutions/sol04_anagrams/anagram.cpp
@@ -5,29 +5,32 @@
 #include <iterator>
 #include <fstream>
 
-using analist=std::set<std::string>;
-analist makeAnagrams(std::string s){
-	sort(s.begin(),s.end());
-	analist anagrams{};
-	anagrams.insert(s);
-	while(std::next_permutation(s.begin(),s.end())){
-		anagrams.insert(s);
-	}
-	return anagrams;
-}
+using analist = std::set<std::string>;
 using outiter=std::ostream_iterator<std::string>;
-void outputRealWordsWithLambda(outiter out,analist const &list){
-	using initer=std::istream_iterator<std::string>;
-	std::ifstream allwords{"linuxwords.txt"};
-	initer input{allwords};
-	copy_if(input,initer{},out,[&list](std::string const& s){return list.count(s);});
+using initer=std::istream_iterator<std::string>;
+
+auto makeAnagrams(std::string s) -> analist{
+  sort(s.begin(),s.end());
+  auto anagrams = analist{};
+  anagrams.insert(s);
+  while(std::next_permutation(s.begin(),s.end())){
+    anagrams.insert(s);
+  }
+  return anagrams;
 }
-int main(){
-	std::string word{};
-	std::cin >> word;
-	auto list=makeAnagrams(word);
-	std::ostream_iterator<std::string> out{std::cout,"\n"};
-	copy(list.begin(),list.end(),out);
-	std::cout << "--------------------------\n";
-	outputRealWordsWithLambda(out,list);
+
+auto outputRealWordsWithLambda(outiter out,analist const &list) -> void{
+  auto allwords = std::ifstream{"linuxwords.txt"};
+  auto input = initer{allwords};
+  copy_if(input,initer{},out,[&list](std::string const& s){return list.count(s);});
+}
+
+auto main() -> int{
+  auto word = std::string{};
+  std::cin >> word;
+  auto list = makeAnagrams(word);
+  auto out = outiter{std::cout,"\n"};
+  copy(list.begin(),list.end(),out);
+  std::cout << "--------------------------\n";
+  outputRealWordsWithLambda(out,list);
 }
--- week08/exercise_solutions/sol04_anagrams_multiwords/anagram_multiword.cpp
+++ week08/exercise_solutions/sol04_anagrams_multiwords/anagram_multiword.cpp
@@ -16,137 +16,141 @@ using anaiter=anawords::const_iterator;
 using anacand=std::vector<anaword>;
 using allanas=std::vector<anacand>;
 using outiter=std::ostream_iterator<std::string>;
-void printCandidate(std::ostream &out, anacand const &cand) {
-	// should create all permutations of cand's words
-	std::vector<std::string> sentence { };
 
-	transform(begin(cand), end(cand), back_inserter(sentence), [](anaword w) {
-		std::string word { };
-		transform(begin(w.second), end(w.second), back_inserter(word), tolower);
-		return word;
-	});
-	copy(begin(sentence), end(sentence), outiter { out, " " });
-	out << '\n';
-}
-void printAllCandidates(std::ostream &out, allanas const &all) {
-	for (auto c : all) {
-		printCandidate(out, c);
-	}
+auto printCandidate(std::ostream &out, anacand const &cand) -> void {
+  // should create all permutations of cand's words
+  auto sentence = std::vector<std::string>{};
+
+  transform(begin(cand), end(cand), back_inserter(sentence), [](anaword w) {
+    auto word = std::string{};
+    transform(begin(w.second), end(w.second), back_inserter(word), tolower);
+    return word;
+  });
+  copy(begin(sentence), end(sentence), outiter { out, " " });
+  out << '\n';
 }
 
-anaword makeAnaword(std::string const &words) {
-	std::string s { words };
-	s.erase(remove_if(begin(s), end(s), [](char c) {
-		return !isalpha(c);
-	}),end(s));
-	transform(begin(s), end(s), begin(s), tolower);
-	return make_pair(anaset { begin(s), end(s) }, words);
+auto printAllCandidates(std::ostream &out, allanas const &all) -> void {
+  for (auto c : all) {
+    printCandidate(out, c);
+  }
 }
 
-void print(std::ostream &out, anaword w) {
-	std::ostream_iterator<char> os { out };
-	copy(begin(w.first), end(w.first), os);
-	out << ": " << w.second << '\n';
+auto makeAnaword(std::string const &words) -> anaword {
+  auto s{words};
+  s.erase(remove_if(begin(s), end(s), [](char c) {
+    return !isalpha(c);
+  }),end(s));
+  transform(begin(s), end(s), begin(s), tolower);
+  return make_pair(anaset { begin(s), end(s) }, words);
 }
 
-bool subsetOfWord(anaword const &w, anaword const &aw) {
-	return aw.second.size() && includes(begin(w.first), end(w.first), begin(aw.first), end(aw.first));
+auto print(std::ostream &out, anaword w) -> void {
+  auto os = std::ostream_iterator<char>{out};
+  copy(begin(w.first), end(w.first), os);
+  out << ": " << w.second << '\n';
 }
 
-bool caselessEqual(anaword const &w, anaword const &v) {
-	return equal(begin(w.second), end(w.second), begin(v.second), end(v.second), [](char l, char r) {
-		return tolower(l) == tolower(r);
-	});
+auto subsetOfWord(anaword const &w, anaword const &aw) -> bool {
+  return aw.second.size() && includes(begin(w.first), end(w.first), begin(aw.first), end(aw.first));
 }
 
-bool notalreadyinSetInDifferentSpelling(anaword const &w, anawords const &wl) {
-	auto b = begin(wl);
-	auto e = end(wl);
-	bool res = e == find_if(b, e, [w](auto v) {
-		return caselessEqual(w, v);
-	});
-	if (!res)
-		std::cout << "already there " << w.second << std::endl;
-	return res;
+auto caselessEqual(anaword const &w, anaword const &v) -> bool {
+  return equal(begin(w.second), end(w.second), begin(v.second), end(v.second), [](char l, char r) {
+    return tolower(l) == tolower(r);
+  });
 }
 
-anawords collectFromFile(anaword w, std::string filename = "linuxwords.txt") {
-	anawords wl;
-	std::ifstream allwords(filename);
-	std::string s;
-	while (getline(allwords, s)) {
-		if (s.size() > 1 || s == "I" || s == "A") {
-			auto aw = makeAnaword(s);
-			if (subsetOfWord(w, aw) && notalreadyinSetInDifferentSpelling(aw, wl)) {
-				wl.push_back(aw);
-
-			}
-		}
-	}
-	return wl;
+auto notalreadyinSetInDifferentSpelling(anaword const &w, anawords const &wl) -> bool {
+  auto b = begin(wl);
+  auto e = end(wl);
+  bool res = e == find_if(b, e, [w](auto v) {
+    return caselessEqual(w, v);
+  });
+  if (!res)
+    std::cout << "already there " << w.second << std::endl;
+  return res;
 }
 
-anaword subtract(anaword const &origin, anaword const &candidate) {
-	if (subsetOfWord(origin, candidate)) {
-		std::string res { };
-		std::set_difference(begin(origin.first), end(origin.first), begin(candidate.first), end(candidate.first), back_inserter(res));
-		return makeAnaword(res);
-	}
-	return origin;
+auto collectFromFile(anaword w, std::string filename = "linuxwords.txt") -> anawords {
+  auto wl = anawords{};
+  auto allwords = std::ifstream{filename};
+  auto s = std::string{};
+
+  while (getline(allwords, s)) {
+    if (s.size() > 1 || s == "I" || s == "A") {
+      auto aw = makeAnaword(s);
+      if (subsetOfWord(w, aw) && notalreadyinSetInDifferentSpelling(aw, wl)) {
+        wl.push_back(aw);
+
+      }
+    }
+  }
+  return wl;
 }
 
-void lookForFittingRest(anawords const &wl, anaiter b, anaiter e, anaword rest, allanas &res, anacand &cand) {
-	while (b != e) {
-		if (subsetOfWord(rest, *b)) {
-			cand.push_back(*b);
-			auto newrest = subtract(rest, *b);
-			if (newrest.first.size()) {
-				lookForFittingRest(wl, begin(wl), e, newrest, res, cand);
-			} else {
-				res.push_back(cand);
-			}
-			cand.pop_back();
-		}
-		++b;
-	}
+auto subtract(anaword const &origin, anaword const &candidate) -> anaword {
+  if (subsetOfWord(origin, candidate)) {
+    auto res = std::string{};
+    std::set_difference(begin(origin.first), end(origin.first), begin(candidate.first), end(candidate.first), back_inserter(res));
+    return makeAnaword(res);
+  }
+  return origin;
 }
 
-void addCandidatesFor(anawords const &wl, anaiter b, anaiter e, anaword origin, allanas &res) {
-	while (b != e) {
-		if (subsetOfWord(origin, *b)) { // should be true anyway
-			anacand cand { 1, *b };
-			auto rest = subtract(origin, *b);
-			if (rest.first.size()) {
-				lookForFittingRest(wl, begin(wl), e, rest, res, cand);
-			} else {
-				res.push_back(cand);
-			}
-		}
-		++b;
-	}
+auto lookForFittingRest(anawords const &wl, anaiter b, anaiter e, anaword rest, allanas &res, anacand &cand) -> void {
+  while (b != e) {
+    if (subsetOfWord(rest, *b)) {
+      cand.push_back(*b);
+      auto newrest = subtract(rest, *b);
+      if (newrest.first.size()) {
+        lookForFittingRest(wl, begin(wl), e, newrest, res, cand);
+      } else {
+        res.push_back(cand);
+      }
+      cand.pop_back();
+    }
+    ++b;
+  }
 }
 
-allanas findCandidates(anawords const &wl, anaword const &origin) {
-	allanas res { };
-	addCandidatesFor(wl, wl.begin(), wl.end(), origin, res);
-	return res;
+auto addCandidatesFor(anawords const &wl, anaiter b, anaiter e, anaword origin, allanas &res) -> void {
+  while (b != e) {
+    if (subsetOfWord(origin, *b)) { // should be true anyway
+      auto cand = anacand{1, *b};
+      auto rest = subtract(origin, *b);
+      if (rest.first.size()) {
+        lookForFittingRest(wl, begin(wl), e, rest, res, cand);
+      } else {
+        res.push_back(cand);
+      }
+    }
+    ++b;
+  }
 }
 
-void doAnagram(std::istream &in, std::ostream &out) {
-	std::string word { };
-	getline(in, word);
-	auto w = makeAnaword(word);
-	print(out, w);
-	auto start = std::chrono::system_clock::now();
-	auto wl = collectFromFile(w, "linuxwords.txt");
-	out << wl.size() << "matching words in set" << std::endl;
-	auto res = findCandidates(wl, w);
-	std::chrono::duration<double> delta = std::chrono::system_clock::now() - start;
-	out << res.size() << " anagrams found\n";
-	out << delta.count() << "s time\n";
-	getline(in, word);
-	printAllCandidates(out, res);
+auto findCandidates(anawords const &wl, anaword const &origin) -> allanas {
+  auto res = allanas{};
+  addCandidatesFor(wl, wl.begin(), wl.end(), origin, res);
+  return res;
 }
-int main() {
-	doAnagram(std::cin, std::cout);
+
+auto doAnagram(std::istream &in, std::ostream &out) -> void {
+  auto word = std::string{};
+  getline(in, word);
+  auto w = makeAnaword(word);
+  print(out, w);
+  auto start = std::chrono::system_clock::now();
+  auto wl = collectFromFile(w, "linuxwords.txt");
+  out << wl.size() << "matching words in set" << std::endl;
+  auto res = findCandidates(wl, w);
+  std::chrono::duration<double> delta = std::chrono::system_clock::now() - start;
+  out << res.size() << " anagrams found\n";
+  out << delta.count() << "s time\n";
+  getline(in, word);
+  printAllCandidates(out, res);
+}
+
+auto main() -> int {
+  doAnagram(std::cin, std::cout);
 }
