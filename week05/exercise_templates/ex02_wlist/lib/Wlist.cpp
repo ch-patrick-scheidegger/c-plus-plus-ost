7685eb7a9ef3577cd3c948649fd4ac8f108881ae
Thomas Corbat <thomas.corbat@ost.ch>
2023-10-19 14:08:39 +0000

    Add wlist template for week 5 as ex2

---
+++ week05/exercise_templates/ex02_wlist/lib/Wlist.cpp
@@ -0,0 +1,7 @@
+#include "Wlist.hpp"
+#include "Word.hpp"
+
+
+auto wlist_caseless(std::istream &input, std::ostream &output) -> void {
+
+}
---
+++ week05/exercise_templates/ex02_wlist/lib/Wlist.hpp
@@ -0,0 +1,8 @@
+#ifndef WLIST_HPP_
+#define WLIST_HPP_
+
+#include <iosfwd>
+
+auto wlist_caseless(std::istream& input, std::ostream& output) -> void;
+
+#endif
