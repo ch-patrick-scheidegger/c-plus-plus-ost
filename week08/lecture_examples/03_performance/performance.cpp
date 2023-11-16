ffe61ac6843e71be82a9119c4941fa6e9c4c82d6
Thomas Corbat <thomas.corbat@ost.ch>
2023-11-16 15:06:08 +0000

    Fix algorithm performance files

--- week08/lecture_examples/03_performance/performance.cpp
+++ week08/lecture_examples/03_performance/performance.cpp
@@ -21,6 +21,11 @@ auto contains_with_algorithm(std::vector<int> const & values, int const v) -> bo
 	return pos != std::end(values);
 }
 
+auto contains_with_ranges(std::vector<int> const & values, int const v) -> bool {
+	auto const pos = std::ranges::find(values, v);
+	return pos != std::end(values);
+}
+
 auto execute_measurement(std::vector<int> const & values, std::ostream & out) -> void {
 	out << std::boolalpha;
 
@@ -35,6 +40,12 @@ auto execute_measurement(std::vector<int> const & values, std::ostream & out) ->
 	auto const algorithm_end = std::chrono::high_resolution_clock::now();
 	auto const algorithm_duration = std::chrono::duration_cast<std::chrono::milliseconds>(algorithm_end - algorithm_start);
 	out << "found with an algorithm: " << algorithm_result << " in " << algorithm_duration.count() << "ms" << std::endl;
+
+	auto const ranges_start = std::chrono::high_resolution_clock::now();
+	auto const ranges_result = contains_with_ranges(values, 2);
+	auto const ranges_end = std::chrono::high_resolution_clock::now();
+	auto const ranges_duration = std::chrono::duration_cast<std::chrono::milliseconds>(ranges_end - ranges_start);
+	out << "found with a range algorithm: " << ranges_result << " in " << ranges_duration.count() << "ms" << std::endl;
 }
 
 auto main() -> int {
