4afc0020d2121d23b965ee7cfc8134bd67d54eb2
Patrick Scheidegger <p.scheidegger1@sunrise.ch>
2023-11-19 22:44:55 +0100

    implement week09 median and rot3

--- week09/exercise_templates/ex01_median/lib/median.hpp
+++ week09/exercise_templates/ex01_median/lib/median.hpp
@@ -2,7 +2,17 @@
 #define MEDIAN_HPP_
 
 
-//TODO Add your definition of the median function template here
+template <typename T>
+auto median(T first, T second, T third) -> T {
+    if((second > first && second < third) || (second < first && second > third)) {
+        return second;
+    }
+
+    if((first > second && first < third) || (first < second && first > third)) {
+        return first;
+    }
+    return third;
+}
 
 
 #endif /* MEDIAN_H_ */
--- week09/exercise_templates/ex02_rotate3arguments/lib/rotate3arguments.hpp
+++ week09/exercise_templates/ex02_rotate3arguments/lib/rotate3arguments.hpp
@@ -3,6 +3,12 @@
 
 
 //TODO Add your definition of the rotate3arguments function template here
-
+template<typename T>
+auto rotate3arguments(T& first, T& second, T& third) -> void {
+    T temp = first;
+    first = second;
+    second = third;
+    third = temp;
+}
 
 #endif
